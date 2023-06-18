83c58fbc81211dedd538a2f34d202ff0998129cc
infizstudent <127938528+infizstudent@users.noreply.github.com>
2023-06-18 13:16:45 +0300

    Add files via upload

--- kalinka/urls.py
+++ kalinka/urls.py
@@ -3,15 +3,17 @@ from django.urls import path, include
 from vebschet import views as vebschet_views
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from vebschet import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', vebschet_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='vebschet/profile.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='vebschet/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='vebschet/logout.html'), name='logout'),
-    path('profile/', vebschet_views.profile, name='profile'),
+    path('profile/<str:username>/', vebschet_views.profile, name='profile'),
+    path('', vebschet_views.index, name='index'),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
-
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
--- vebschet/views.py
+++ vebschet/views.py
@@ -1,37 +1,47 @@
-from django.contrib.auth.decorators import login_required
-from .models import UserProfile
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .forms import UserProfileForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def profile(request):
+    user_profile = request.user.userprofile
+    return render(request, 'vebschet/profile.html', {'user_profile': user_profile})
+
+
+def index(request):
+    return render(request, 'vebschet/index.html')
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-        if form.is_valid() and profile_form.is_valid():
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
             user = form.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            return redirect('profile')
+            UserProfile.objects.create(user=user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт создан для {username}! Вы можете войти в систему.')
+            return redirect('profile', username=username)
     else:
-        form = UserCreationForm()
-        profile_form = UserProfileForm()
-    return render(request, 'vebschet/registration.html', {'form': form, 'profile_form': profile_form})
-
+        form = UserRegisterForm()
+    return render(request, 'vebschet/register.html', {'form': form})
 
 @login_required
-def profile(request):
-    user = request.user
+def profile(request, username):
+    user = User.objects.get(username=username)
     profile = UserProfile.objects.get(user=user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect('profile')
+            return redirect('profile', username=username)
     else:
         form = UserProfileForm(instance=profile)
 
-    return render(request, 'vebschet/profile.html', {'user': user, 'form': form})
+    return render(request, 'users/profile.html', {'form': form})
+
